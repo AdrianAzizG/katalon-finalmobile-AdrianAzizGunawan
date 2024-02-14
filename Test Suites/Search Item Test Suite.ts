bf565f7def0fed1cd8ae3f76630181ec3fdf30f6
aziz_ <azizadrian9@gmail.com>
2024-02-14 14:36:23 +0700

    Create Test Suite Case for Search Item

---
+++ Test Suites/Search Item Test Suite.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Search Item Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>dba9a11c-6d2b-458f-a05d-f65752309f73</testSuiteGuid>
+   <testCaseLink>
+      <guid>ff29822e-094a-4ae8-9bcc-a71728a022ef</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Search Item 01 - Sukses</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>f522d903-03fa-4c34-af84-734d7e473deb</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Users</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>f522d903-03fa-4c34-af84-734d7e473deb</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>625263bc-25ed-4ade-91d1-8b686a225fad</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>f522d903-03fa-4c34-af84-734d7e473deb</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>6fde25ed-595b-4b11-ae74-d8fcb4e444e6</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>22fc566e-b51f-4563-b116-09dfc3bfb3f9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Search Item 02 - Item Not Found</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
